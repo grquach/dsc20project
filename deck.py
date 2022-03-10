4d4402f4dd30ed1dac5e3d8806aa1ed26fd1734a
ammos135 <101073537+ammos135@users.noreply.github.com>
2022-03-10 15:13:47 -0800

    first run through

--- deck.py
+++ deck.py
@@ -26,7 +26,9 @@ class Deck:
         """
         Creates a Deck instance containing cards sorted in ascending order.
         """
-        ...
+        ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+        suits = ['clubs', 'diamonds', 'hearts', 'spades']
+        self.cards = [Card(rank, suit) for rank in ranks for suit in suits]
 
     def shuffle(self, **shuffle_and_count):
         """Shuffles the deck using a variety of different shuffles.
@@ -36,13 +38,26 @@ class Deck:
             shuffle type and the number of times the shuffled
             should be called.
         """
-        ...
+        try:
+            num_modified_overhand = shuffle_and_count['modified_overhand']
+        except:
+            num_modified_overhand = 0
+        try:
+            num_mongean = shuffle_and_count['mongean']
+        except:
+            num_mongean = 0
+        self.cards = Shuffle.modified_overhand(self.cards, num_modified_overhand)
+        for j in range(num_mongean):
+            self.cards = Shuffle.mongean(self.cards)
 
     def deal_hand(self, hand):
         """
         Takes the first card from the deck and adds it to `hand`.
         """
-        ...
+        self.hand = hand
+        assert isinstance(self.hand, PlayerHand)
+        self.hand.add_card(self.cards[0])
+        self.cards.pop(0)
 
     def get_cards(self):
-        ...
+        return self.cards
